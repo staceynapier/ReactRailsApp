5b6869f725e176c36f35b61e952a3de35a8d1b99
staceynapier <staceynapier86@gmail.com>
2017-10-05 13:45:55 +0100

    json data can now be viewed at localhost:5000/properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,7 +4,7 @@ class PropertiesController < ApplicationController
     params.require(:show).permit([:location, :bedrooms, :publicRooms, :bathrooms, :price, :description, :furnished, :images])
   end
 
-  def index
+  def show
     properties = Property.all
     render :json => properties
   end
